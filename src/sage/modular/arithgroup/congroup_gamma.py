6233c1b0374a25cc0c9e5906b71c0382d394d043
Frédéric Chapoton <chapoton@unistra.fr>
2026-01-15 09:22:49 +0100

    remove deprecated stuff in modular/arithgroup

--- src/sage/modular/arithgroup/congroup_gamma.py
+++ src/sage/modular/arithgroup/congroup_gamma.py
@@ -64,7 +64,7 @@ class Gamma_class(CongruenceSubgroup):
     r"""
     The principal congruence subgroup `\Gamma(N)`.
     """
-    def _repr_(self):
+    def _repr_(self) -> str:
         """
         Return the string representation of ``self``.
 
@@ -75,7 +75,7 @@ class Gamma_class(CongruenceSubgroup):
         """
         return "Congruence Subgroup Gamma(%s)" % self.level()
 
-    def _latex_(self):
+    def _latex_(self) -> str:
         r"""
         Return the \LaTeX representation of ``self``.
 
@@ -307,23 +307,6 @@ class Gamma_class(CongruenceSubgroup):
         return MatrixGroup([matrix(Zmod(self.level()), 2, 2, 1)])
 
 
-def is_Gamma(x):
-    r"""
-    Return ``True`` if x is a congruence subgroup of type Gamma.
-
-    EXAMPLES::
-
-        sage: from sage.modular.arithgroup.congroup_gamma import Gamma_class
-        sage: isinstance(Gamma0(13), Gamma_class)
-        False
-        sage: isinstance(Gamma(4), Gamma_class)
-        True
-    """
-    from sage.misc.superseded import deprecation
-    deprecation(38035, "The function is_Gamma is deprecated; use 'isinstance(..., Gamma_class)' instead.")
-    return isinstance(x, Gamma_class)
-
-
 def _lift_pair(U, V, N):
     r"""
     Utility function. Given integers ``U, V, N``, with `N \ge 1` and `{\rm
@@ -350,7 +333,7 @@ def _lift_pair(U, V, N):
     v = V % N
     if v == 0:
         if u == 1 or u == N-1:
-            return (1,0)
+            return (1, 0)
         else:
             v = N
     while gcd(u, v) > 1:
